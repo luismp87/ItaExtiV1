b811b99f506fc9d1c60270a8d4c82e387e357c36
luismp87 <luismp87@gmail.com>
2016-07-06 11:23:59 -0500

    SS
    SS

--- js/actions.js
+++ js/actions.js
@@ -5,10 +5,11 @@ var fn = {
 	init: function(){
 		// FUNCION PARA INICIO
 		window.location.href = '#inicio';
-		$('#btnautentificar').tap(fn.autentificar);
+		$('#btnautentificar').click(fn.autentificar);
         //tap
-        $('#btnleercodigo').tap(fn.leerCodigoDeBarras);	
-        $('#btnprueba').tap(fn.prueba);
+        $('#btnleercodigo').click(fn.leerCodigoDeBarras);
+        $('#btnbuscar_info_extintor').click(fn.buscar_info_extintor);	        
+
 	},
 	autentificar: function(){         
 		var nom = $('#txtusuario').val();
@@ -29,20 +30,20 @@ var fn = {
                             }
                         else
                             {
-                            navigator.notification.alert("Usuario o contraseña incorrectos",null,"Error al Ingresar","Aceptar");   
-                            //alert("Usuario o contraseña incorrectos");
+                            //navigator.notification.alert("Usuario o contraseña incorrectos",null,"Error al Ingresar","Aceptar");   
+                            alert("Usuario o contraseña incorrectos");
                             }                        
                     });					
                 },
 				error: function(jq, txt){
-					//alert(jq + txt.responseText);
-                    navigator.notification.alert(jq + txt.responseText,null,"Error al Ingresar","Aceptar");
+					alert(jq + txt.responseText);
+                    //navigator.notification.alert(jq + txt.responseText,null,"Error al Ingresar","Aceptar");
 				}
 			});
 		}
 		else{
-			navigator.notification.alert("Todos Los Campos Son Requeridos",null,"Error al Ingresar","Aceptar");
-			//alert("todos los campos son requeridos");
+			//navigator.notification.alert("Todos Los Campos Son Requeridos",null,"Error al Ingresar","Aceptar");
+			alert("todos los campos son requeridos");
 		}	
     },
     leerCodigoDeBarras: function(){
@@ -52,14 +53,47 @@ var fn = {
                             $("#txtitaextiV1").val("" + result.text); 
 		  }, 
 		  function (error) {
-              navigator.notification.alert("Scanning failed: " + error,null,"Error","Aceptar");
-			  //alert("Scanning failed: " + error);
+              //navigator.notification.alert("Scanning failed: " + error,null,"Error","Aceptar");
+			  alert("Scanning failed: " + error);
 		  }
 	   );
 	},
-    prueba: function(){        
-        $("#txtprueba").val("ss"); 
+    buscar_info_extintor : function(){         
+		var id = $('#txtitaextiV1').val();		
+		if(id != ''){	
+			$.mobile.loading("show",{theme: 'b'});
+			$.ajax({
+                method: 'POST',
+				url: 'http://servidoriis.laitaliana.com.mx/LM/wsitaextiv1/Service1.asmx/buscar_info_extintor',				
+                data: {id: id},
+                dataType: "json",
+				success: function (msg){
+					$.mobile.loading("hide");
+                    $.each(msg,function(i,item){
+                        if(msg[i].valor1 = "encontro")
+                            {
+                            $("#pID").text(msg[i].ID);
+                            $("#pUBICACION").text(msg[i].UBICACION);
+
+                            }
+                        else
+                            {
+                            //navigator.notification.alert("Usuario o contraseña incorrectos",null,"Error al Ingresar","Aceptar");   
+                            alert("Usuario o contraseña incorrectos");
+                            }                        
+                    });					
+                },
+				error: function(jq, txt){
+					alert(jq + txt.responseText);
+                    //navigator.notification.alert(jq + txt.responseText,null,"Error al Ingresar","Aceptar");
+				}
+			});
+		}
+		else{
+			//navigator.notification.alert("Ingrese el ID del extintor",null,"Error al Ingresar","Aceptar");
+			alert("Ingrese el ID del extintor");
+		}	
     }
 };
-$(fn.ready);
-//$(fn.init);
+//$(fn.ready);
+$(fn.init);
