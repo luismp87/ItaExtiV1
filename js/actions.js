724cdab5f1f71205f2db2871c328826428a78ef2
luismp87 <luismp87@gmail.com>
2016-07-25 16:52:24 -0500

    ss
    ss

--- js/actions.js
+++ js/actions.js
@@ -3,20 +3,31 @@ var fn = {
 		document.addEventListener("deviceready",fn.init,false);
 	},
 	init: function(){
-		window.location.href = '#inicio';
+        if(!fn.estaRegistrado())
+            window.location.href = '#inicio';
+		//window.location.href = '#inicio';
 		// LOGIO EN EL SERVIDOR --> $('#btnautentificar').tap(fn.autentificar);
         $('#btnautentificar').tap(fn.autentificarJSON);
         $('#btnleercodigo').tap(fn.leerCodigoDeBarras);
         $('#btnbuscar_info_extintor').tap(fn.buscar_info_extintor);	        
 		//$('#btnprueba').tap(fn.myFunction);
-        $('#btnGuardarDB').tap(fn.GuardarDB);
-        $('#btnMostrarDB').tap(fn.MostrarDB);
         $('#btnActualizarBDDesdeServer').tap(fn.ActualizarBDDesdeServer);
         $('#btnMigrarExtintoresRM').tap(fn.MigrarExtintoresRM);
         $('#btnEliminarExtintores').tap(fn.EliminarExtintores);
+        $('.ablanqueaCext1').tap(fn.blanqueaCext1);
+        $('#capturaExt2 div[data-role=footer] #btnGuardarRegExt').tap(fn.GuardarRegExt);
+        $('#btniraRegistrarEXT').tap(fn.iraRegistrarEXT);
+        $('#btncerrarsesion').tap(fn.cerrarsesion);
         
         
 	},
+     estaRegistrado: function(){
+        var usr = window.localStorage.getItem("user");
+        if(usr == undefined || usr == '')
+            return false;
+        else
+            return true;
+    },
 	autentificar: function(){         
 		var nom = $('#txtusuario').val();
 		var passw = $('#txtcontrasena').val();
@@ -31,7 +42,7 @@ var fn = {
 					$.mobile.loading("hide");
                     $.each(msg,function(i,item){
                         if(msg[i].valor1 = "correcto")
-                            {
+                            {                           
                             window.location.href = '#TiposDeCaptura';
                             }
                         else
@@ -85,6 +96,8 @@ var fn = {
     //alert("hola1");
     for(i = 0; i<usuarios.length; i++) {
         if(( usuarios[i].usuario == usuariof) && (usuarios[i].pass == passf)){
+        window.localStorage.setItem("user",usuariof);
+        //alert(""+usuariof);
         window.location.href = '#TiposDeCaptura';
         encontrado = "true";
         break;
@@ -108,28 +121,12 @@ var fn = {
     frecarga: '',
     ffabricacion: '',
     fproxservicio: '',
-    GuardarDB : function() {
-
-            fn.id_ext = $('#txtusuario').val(); 
-            fn.ubicacion= $('#txtusuario').val(); 
-            fn.capacidad= $('#txtusuario').val(); 
-            fn.clase= $('#txtusuario').val(); 
-            fn.agente= $('#txtusuario').val(); 
-            fn.marca= $('#txtusuario').val(); 
-            fn.frecarga= $('#txtusuario').val(); 
-            fn.ffabricacion= $('#txtusuario').val(); 
-            fn.fproxservicio= $('#txtusuario').val(); 
-            almacen.guardarEXT(fn.id_ext, fn.ubicacion,fn.capacidad,fn.clase,fn.agente,fn.marca,fn.frecarga,fn.ffabricacion,fn.fproxservicio);
-    },
-    MostrarDB : function(){
-        navigator.notification.alert("ss" +almacen.leerExtintor(),null,"Error al Ingresar","Aceptar");  
-        //almacen.leerExtintor();
-    },
     ActualizarBDDesdeServer :function(){
         almacen.leerExt();  
         window.location.href = '#RemotaALocal';
     },
-    MigrarExtintoresRM : function(){       
+    MigrarExtintoresRM : function(){ 
+        var myArray = new Array(500); 
         var registros = $('#NumDeExtintores').val();  
         if(registros == 0)
             {
@@ -142,20 +139,9 @@ var fn = {
                 success: function (msg){
                     $.mobile.loading("hide");
                     $.each(msg,function(i,item){
-
-                    fn.id_ext =  msg[i].ID_EXT;
-                    fn.ubicacion= msg[i].UBICACION;
-                    fn.capacidad= msg[i].CAPACIDAD;
-                    fn.clase= msg[i].CLASE;
-                    fn.agente= msg[i].AGENTE;
-                    fn.marca= msg[i].MARCA;
-                    fn.frecarga= msg[i].FRECARGA;
-                    fn.ffabricacion= msg[i].FFABRICACION;
-                    fn.fproxservicio= msg[i].FPROXSERVICIO;
-                    
-                    almacen.guardarEXT(fn.id_ext, fn.ubicacion,fn.capacidad,fn.clase,fn.agente,fn.marca,fn.frecarga,fn.ffabricacion,fn.fproxservicio);
-
+                        myArray[i] = msg[i].ID_EXT + "','" + msg[i].UBICACION + "','" + msg[i].CAPACIDAD+ "','" + msg[i].CLASE+ "','" + msg[i].AGENTE+ "','" + msg[i].MARCA+ "','" + msg[i].FRECARGA+ "','" + msg[i].FFABRICACION+ "','" + msg[i].FPROXSERVICIO+ "','" + msg[i].PLANTA;
                     }); 
+                    almacen.guardarEXT(fn.id_ext, fn.ubicacion,fn.capacidad,fn.clase,fn.agente,fn.marca,fn.frecarga,fn.ffabricacion,fn.fproxservicio,myArray);
                     almacen.leerExt();  
                     navigator.notification.alert("Migración Correcta",null,"Listo","Aceptar");               
         },
@@ -170,13 +156,156 @@ var fn = {
                     }
                     else
                     {
-                       navigator.notification.alert("Se tienen registros en la base de datos, antes eliminelos",null,"Error al Ingresar","Aceptar");    
+                       navigator.notification.alert("Se tienen registros en la base de datos, antes eliminelos",null,"Advertencia","Aceptar");    
                     }
         },
         EliminarExtintores : function(){
             almacen.eliminarExt();
             almacen.leerExt();  
+        },
+        blanqueaCext1: function(){
+                            $("#pPLANTA").text("");
+                            $("#pUBICACION").text("");
+                            $("#pCAPACIDAD").text("");
+                            $("#pCLASE").text("");
+                            $("#pAGENTE").text("");
+                            $("#pMARCA").text("");
+                            $("#pFRECARGA").text("");
+                            $("#pFFABRICACION").text("");
+                            $("#pFPROXSERVICIO").text("");              
+        },
+            presion: '',
+            manometro: '',
+            segurosello: '',
+            manguera: '',
+            soporte: '',
+            pintura: '',
+            valvula: '',
+            cilindro: '',
+            nemotecnia: '',
+            senalamiento: '',
+            gabinete: '',
+            observaciones: '',
+            usuario: '',
+        GuardarRegExt: function(){
+            
+           
+            fn.id_ext = $('#txtitaextiV1').val();
+            fn.presion= $('#textPRESION').val();
+            fn.manometro = $('#textMANOMETRO').val();
+            fn.segurosello = $('#textSEGUROSELLO').val();
+            fn.manguera = $('#textMANGUERA').val();
+            fn.soporte = $('#textSOPORTE').val();
+            fn.pintura = $('#textPINTURA').val();
+            fn.valvula = $('#textVALVULA').val();
+            fn.cilindro = $('#textCILINDRO').val();
+            fn.nemotecnia = $('#textNEMOTECNIA').val();
+            fn.senalamiento = $('#textSENALAMIENTO').val();
+            fn.gabinete = $('#textGABINETE').val();
+            fn.observaciones = $('#textOBSERVACIONES').val();    
+            fn.usuario = window.localStorage.getItem("user");
+
+            if((fn.presion != "0") &&              
+            (fn.manometro != "0") &&  
+            (fn.segurosello != "0") &&  
+            (fn.manguera != "0") &&  
+            (fn.soporte != "0") &&  
+            (fn.pintura != "0") &&  
+            (fn.valvula != "0") &&  
+            (fn.cilindro != "0") &&  
+            (fn.nemotecnia != "0") &&  
+            (fn.senalamiento != "0") &&  
+            (fn.gabinete != "0"))
+          { 
+                        if(navigator.connection.type != Connection.NONE)
+                        {
+                             
+/******GUARDAMOS EN EL SERVIDOR SI SE TIENE SEÑAL******/
+/*$.ajax({
+                method: 'POST',
+                url: 'http://servidoriis.laitaliana.com.mx/LM/wsitaextiv1/Service1.asmx/insertarregextintores',              
+                data: {usuario: nom, contrasena: passw},
+                dataType: "json",
+                success: function (msg){
+                    $.mobile.loading("hide");
+                    $.each(msg,function(i,item){
+                        if(msg[i].valor1 = "correcto")
+                            {                           
+                            window.location.href = '#TiposDeCaptura';
+                            }
+                        else
+                            {
+                            navigator.notification.alert("Usuario o contraseña incorrectos",null,"Error al Ingresar","Aceptar");   
+                            //alert("Usuario o contraseña incorrectos");
+                            }                        
+                    });                 
+                },
+                error: function(jq, txt){
+                    //alert(jq + txt.responseText);
+                    navigator.notification.alert(jq + txt.responseText,null,"Error al Ingresar","Aceptar");
+                }
+            });*/
+/**************/
+                             navigator.notification.alert("Segun se manda al server.",null,"Advertencia","Aceptar");
+                            //server.sincronizar(fn.per,fn.dia,fn.th);//Enviar a servidor
+                        }
+                        else
+                        {
+                                almacen.guardarRegistroEXT(fn.id_ext,fn.presion,fn.manometro,fn.segurosello,fn.manguera,fn.soporte,fn.pintura,fn.valvula,fn.cilindro,fn.nemotecnia,fn.senalamiento,fn.gabinete,fn.observaciones,fn.usuario);
+                                $("#textPRESION").val("0").change();
+                                $("#textMANOMETRO").val("0").change();
+                                $("#textSEGUROSELLO").val("0").change();
+                                $("#textMANGUERA").val("0").change();
+                                $("#textSOPORTE").val("0").change();
+                                $("#textPINTURA").val("0").change();
+                                $("#textVALVULA").val("0").change();
+                                $("#textCILINDRO").val("0").change();
+                                $("#textNEMOTECNIA").val("0").change();
+                                $("#textSENALAMIENTO").val("0").change();
+                                $("#textGABINETE").val("0").change();
+                                $("#textOBSERVACIONES").val("");
+                                window.location.href = '#TiposDeCaptura';
+                        }
+          }
+          else
+          {
+            navigator.notification.alert("Tiene características sin seleccionar.",null,"Advertencia","Aceptar");    
+          }
+           
+
+            //navigator.notification.alert("Datos 2: " + fn.id_ext + " - " + fn.presion + " - " + fn.manometro + " - " + fn.segurosello + " - " + fn.manguera + " - " + fn.soporte + " - " + fn.pintura + " - " + fn.valvula + " - " + fn.cilindro + " - " + fn.nemotecnia + " - " + fn.senalamiento + " - " + fn.gabinete + " - " + fn.observaciones,null,"Advertencia","Aceptar");    
+        },
+        iraRegistrarEXT: function(){         
+            var planta = $("#pPLANTA").text();
+            if(planta.length <= 0)
+            { 
+                 navigator.notification.alert("Escanee o ingrese el ID del Extintor",null,"Advertencia","Aceptar");  
+                window.location.href = '#capturaExt1';
+            } 
+            else
+            {
+                $("#textPRESION").val("0").change();
+                $("#textMANOMETRO").val("0").change();
+                $("#textSEGUROSELLO").val("0").change();
+                $("#textMANGUERA").val("0").change();
+                $("#textSOPORTE").val("0").change();
+                $("#textPINTURA").val("0").change();
+                $("#textVALVULA").val("0").change();
+                $("#textCILINDRO").val("0").change();
+                $("#textNEMOTECNIA").val("0").change();
+                $("#textSENALAMIENTO").val("0").change();
+                $("#textGABINETE").val("0").change();
+                $("#textOBSERVACIONES").val("");
+                window.location.href = '#capturaExt2';
+            }        
+        },
+        cerrarsesion: function(){
+        window.localStorage.setItem("user",'');    
+        window.location.href = '#inicio';
+
         }
+
+
 
 };
 $(fn.ready);
--- js/almacenamiento.js
+++ js/almacenamiento.js
@@ -1,4 +1,5 @@
 var almacen = {
+	/*VARIABLES SOBRE EL EXTINTOR*/
 	id_ext: null,
 	ubicacion: null,
 	capacidad: null,
@@ -8,12 +9,32 @@ var almacen = {
 	frecarga: null,
 	ffabricacion: null,
 	fproxservicio: null,
+	planta: null,
+/*VARIABLES SOBRE CARACTERISTICAS DEL EXTINTOR*/
+presion: null,
+manometro: null,
+segurosello: null,
+manguera: null,
+soporte: null,
+pintura: null,
+valvula: null,
+cilindro: null,
+nemotecnia: null,
+senalamiento: null,
+gabinete: null,
+observaciones: null,
+usuario: null,
+fechaderegistro: null,
+
+
+
+	myArray: null,
 
 	numerodefilas : 0,
 
 	db: null,
 	/*FUNCION PARA GUARDAR EN BASE DE DATOS*/
-	guardarEXT: function(id_ext, ubicacion,capacidad,clase,agente,marca,frecarga,ffabricacion,fproxservicio){
+	guardarEXT: function(id_ext, ubicacion,capacidad,clase,agente,marca,frecarga,ffabricacion,fproxservicio,myArray,planta){
 		almacen.id_ext = id_ext;
 		almacen.ubicacion = ubicacion;
 		almacen.capacidad = capacidad;
@@ -23,18 +44,25 @@ var almacen = {
 		almacen.frecarga = frecarga;
 		almacen.ffabricacion = ffabricacion;
 		almacen.fproxservicio = fproxservicio;
-			
+		almacen.planta = planta;
+		almacen.myArray	= myArray;        
 			almacen.db = window.openDatabase("ItaExtiV1DB","1.0","ItaExtiV1 Storage",20000);
 			almacen.db.transaction(almacen.GuardarExtintor, almacen.error, null);
 			
 		},
 									GuardarExtintor: function(tx){
-										tx.executeSql("CREATE TABLE IF NOT EXISTS ita_sh_extintores (id_ext, ubicacion,capacidad,clase,agente,marca,frecarga,ffabricacion,fproxservicio)");
-										tx.executeSql("INSERT INTO ita_sh_extintores (id_ext, ubicacion,capacidad,clase,agente,marca,frecarga,ffabricacion,fproxservicio) VALUES ('"+almacen.id_ext+"','"+almacen.ubicacion+"','"+almacen.capacidad+"','"+almacen.clase+"','"+almacen.agente+"','"+almacen.marca+"','"+almacen.frecarga+"','"+almacen.ffabricacion+"','"+almacen.fproxservicio+"')");
-										navigator.notification.alert(""+ almacen.id_ext,null,"ss","Aceptar");
+										tx.executeSql("CREATE TABLE IF NOT EXISTS ita_sh_extintores (id_ext, ubicacion,capacidad,clase,agente,marca,frecarga,ffabricacion,fproxservicio,planta)");
+										    //navigator.notification.alert("longitud " +almacen.myArray.length ,null,"Listo","Aceptar");      
+										    for(i = 0; i<almacen.myArray.length; i++) 
+										    {
+										    	if((almacen.myArray[i] != "") && (almacen.myArray[i] != undefined))
+										    	{
+										    		tx.executeSql("INSERT INTO ita_sh_extintores (id_ext, ubicacion,capacidad,clase,agente,marca,frecarga,ffabricacion,fproxservicio,planta) VALUES ('"+almacen.myArray[i]+"')");
+    											}
+        									}        
 									},
 									CreaSINOExiste: function(tx){
-										tx.executeSql("CREATE TABLE IF NOT EXISTS ita_sh_extintores (id_ext, ubicacion,capacidad,clase,agente,marca,frecarga,ffabricacion,fproxservicio)");										
+										tx.executeSql("CREATE TABLE IF NOT EXISTS ita_sh_extintores (id_ext, ubicacion,capacidad,clase,agente,marca,frecarga,ffabricacion,fproxservicio,planta)");										
 									},
 									error: function(){
 										//alert("Error al acceder a la Base de Datos");
@@ -43,6 +71,10 @@ var almacen = {
 									Correcto: function(){
 										//alert("Reserva guardada en espera de sincronización");
 										navigator.notification.alert("Ejecución satisfactoria", null, "Correcto", "Aceptar");
+									},
+									GuardadoCorrectoLocalEXT: function(){										
+										navigator.notification.alert("Se guardo la información en el dispositivo", null, "Correcto", "Aceptar");										
+										
 									},
 	/*FUNCION PARA LEER EN BASE DE DATOS*/
 	leerExt: function(){
@@ -73,6 +105,7 @@ var almacen = {
 										});
 										
 		},
+/*FUNCION PARA ELIMINAR EN BASE DE DATOS*/
 		eliminarExt: function(tx){
 			almacen.db = window.openDatabase("ItaExtiV1DB","1.0","ItaExtiV1 Storage",20000);
 			almacen.db.transaction(almacen.CreaSINOExiste, almacen.error, null);
@@ -81,6 +114,7 @@ var almacen = {
 									eliminarExtintores: function(tx){
 									tx.executeSql("DELETE FROM ita_sh_extintores");
 	},
+/*FUNCION PARA LEER EN BASE DE DATOS*/
 		leerinformacionEXT: function(tx){
 			almacen.db = window.openDatabase("ItaExtiV1DB","1.0","ItaExtiV1 Storage",20000);
 			almacen.db.transaction(almacen.CreaSINOExiste, almacen.error, null);
@@ -88,9 +122,12 @@ var almacen = {
 
 	},
 									leerinfoEXT: function(tx){
-									tx.executeSql("SELECT id_ext, ubicacion,capacidad,clase,agente,marca,frecarga,ffabricacion,fproxservicio FROM ita_sh_extintores", [], function(tx2, t){
+										
+									tx.executeSql("SELECT id_ext, ubicacion,capacidad,clase,agente,marca,frecarga,ffabricacion,fproxservicio,planta FROM ita_sh_extintores where upper(id_ext) = upper('" +$('#txtitaextiV1').val()+ "')", [], function(tx2, t){
+									var encontroEXT = 0;
 											for(i = 0; i < t.rows.length; i++){
-
+							encontroEXT= 1;
+							$("#pPLANTA").text(t.rows.item(i).planta);
 							$("#pUBICACION").text(t.rows.item(i).ubicacion);
                             $("#pCAPACIDAD").text(t.rows.item(i).capacidad);
                             $("#pCLASE").text(t.rows.item(i).clase);
@@ -110,11 +147,42 @@ var almacen = {
 																			  }, "Tabla Reservas","Vibrar,Sonar,Cancelar");*/
 												//server.sincronizar(t.rows.item(i).pr,t.rows.item(i).di,t.rows.item(i).th);
 												//alert("id_ext: " + t.rows.item(i).id_ext);
-												navigator.notification.alert("ubicacion: " + t.rows.item(i).id_ext, null, "Correcto", "Aceptar");
+												//navigator.notification.alert("ubicacion: " + t.rows.item(i).id_ext, null, "Correcto", "Aceptar");
 											}
 
+	if(encontroEXT == 0)
+	{
+		navigator.notification.alert("Sin resultados verifique el ID del Extintor", null, "Advertencia", "Aceptar");
+	}
 //navigator.notification.alert("almacen.numerodefilas: " + almacen.numerodefilas, null, "Correcto", "Aceptar");
 										});
-	}
+	
+	},
+/*FUNCION PARA GUARDAR LA INFORMACION DE LO QUE SE CAPTURA SOBRE EXTINTORES EN BASE DE DATOS*/
+		guardarRegistroEXT: function(id_ext,presion,manometro,segurosello,manguera,soporte,pintura,valvula,cilindro,nemotecnia,senalamiento,gabinete,observaciones,usuario){
+		almacen.id_ext = id_ext;
+		almacen.presion=presion;
+		almacen.manometro=manometro;
+		almacen.segurosello=segurosello;
+		almacen.manguera=manguera;
+		almacen.soporte=soporte;
+		almacen.pintura=pintura;
+		almacen.valvula=valvula;
+		almacen.cilindro=cilindro;
+		almacen.nemotecnia=nemotecnia;
+		almacen.senalamiento=senalamiento;
+		almacen.gabinete=gabinete;
+		almacen.observaciones=observaciones;
+		almacen.usuario = usuario;
+		almacen.fechaderegistro = d.getDate() + "/" + (d.getMonth() +1) + "/" + d.getFullYear(), ' '+d.getHours(),':'+d.getMinutes(),':'+d.getSeconds();
+			almacen.db = window.openDatabase("ItaExtiV1DB","1.0","ItaExtiV1 Storage",20000);
+			almacen.db.transaction(almacen.GuardarRegistroExtintor, almacen.error, almacen.GuardadoCorrectoLocalEXT);
+			
+		},
+									GuardarRegistroExtintor: function(tx){
+										tx.executeSql("CREATE TABLE IF NOT EXISTS ita_sh_reg_ext (id_ext,presion,manometro,segurosello,manguera,soporte,pintura,valvula,cilindro,nemotecnia,senalamiento,gabinete,observaciones,usuario,fechaderegistro)");
+										tx.executeSql("INSERT INTO ita_sh_reg_ext (id_ext,presion,manometro,segurosello,manguera,soporte,pintura,valvula,cilindro,nemotecnia,senalamiento,gabinete,observaciones,usuario,fechaderegistro) VALUES ('"+almacen.id_ext+"','"+almacen.presion+"','"+almacen.manometro+"','"+almacen.segurosello+"','"+almacen.manguera+"','"+almacen.soporte+"','"+almacen.pintura+"','"+almacen.valvula+"','"+almacen.cilindro+"','"+almacen.nemotecnia+"','"+almacen.senalamiento+"','"+almacen.gabinete+"','"+almacen.observaciones+"','"+almacen.usuario+"','"+almacen.fechaderegistro+"')");       
+										alert(""+ almacen.fechaderegistro);
+									}
 
 }
