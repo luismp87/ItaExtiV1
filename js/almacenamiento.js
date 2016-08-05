7f3627636dab31699e5b2344d29aff20673bf554
luismp87 <luismp87@gmail.com>
2016-08-05 11:02:05 -0500

    ss
    ss

--- js/almacenamiento.js
+++ js/almacenamiento.js
@@ -215,7 +215,7 @@ fechaderegistro: null,
 
 	},
 									leerinforegistradaEXT: function(tx){
-									//server.iniciarcontador1();										
+									server.iniciarcontador1();										
 									tx.executeSql("SELECT id_ext,presion,manometro,segurosello,manguera,soporte,pintura,valvula,cilindro,nemotecnia,senalamiento,gabinete,observaciones,usuario,fechaderegistro FROM ita_sh_reg_ext", [], function(tx2, t){
 									var encontroEXT = 0;
 											for(i = 0; i < t.rows.length; i++){
