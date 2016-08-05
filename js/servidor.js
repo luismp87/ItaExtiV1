86019cc1d3f88a31525e75e32485c547ec368027
luismp87 <luismp87@gmail.com>
2016-08-05 09:35:09 -0500

    ss
    ss

--- js/servidor.js
+++ js/servidor.js
@@ -142,8 +142,9 @@ $.ajax({
                            //navigator.notification.alert("La información se envio al servidor de forma correcta",null,"Advertencia","Aceptar");   
                            //navigator.notification.alert(id_ext+"-" +presion+"-" +manometro+"-" +segurosello+"-" +manguera+"-" +soporte+"-" +pintura+"-" +valvula+"-" +cilindro+"-" +nemotecnia+"-" +senalamiento+"-" +gabinete+"-" +observaciones+"-" +usuario+"-" +fechaderegistro ,null,"Error 126","Aceptar");                           
                            server.myArray[server.contador1] = almacen.id_ext+"' and fechaderegistro= '"+ almacen.fechaderegistro;   // msg[i].ID_EXT + "','" + msg[i].UBICACION + "','" + msg[i].CAPACIDAD+ "','" + msg[i].CLASE+ "','" + msg[i].AGENTE+ "','" + msg[i].MARCA+ "','" + msg[i].FRECARGA+ "','" + msg[i].FFABRICACION+ "','" + msg[i].FPROXSERVICIO+ "','" + msg[i].PLANTA;
-                           server.contador1 =  server.contador1 + 1;
+                           
                            navigator.notification.alert("contador 1:" + server.contador1 + " array:" + server.myArray[server.contador1],null,"Advertencia","Aceptar");   
+                           server.contador1 =  server.contador1 + 1;
                            //almacen.eliminarregistrosExt(id_ext,fechaderegistro);
                             }
                         else
